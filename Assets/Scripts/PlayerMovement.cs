7dd790d5538565780a40547db6887a9ceae1dc11
whqlskqTy2 <kimhhss06@gmail.com>
2025-04-16 15:56:48 +0900

    [Feat] 플레이어 움직임 개선,코요테 타임추가,카메라 Follow

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("ê¸°ë³¸ ì´ë™ ì„¤ì •")]
     public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    public float jumpForce = 7f;
+    public float turnSpeed = 10f;
+
+    [Header("ì í”„ ê°œì„  ì„¤ì •")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2.0f;
+
+    [Header("ì§€ë©´ ê°ì§€ ì„¤ì •")]
+    public float coyoteTime = 0.15f;
+    public float coyoteTimeCounter;
+    public bool realGround = true;
+
+    [Header("ê¸€ë¼ì´ë” ì„¤ì •")]
+    public GameObject gliderObject;
+    public float glidarFallSpeed = 1.0f;
+    public float gliderMoveSpeed = 7.0f;
+    public float gliderMaxTime = 5.0f;
+    public float gliderTimeLeft;
+    public bool isGliding = false;
 
     public bool isGrounded = true;
 
@@ -19,25 +38,125 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (gliderObject != null)
+        {
+            gliderObject.SetActive(false);
+        }
+
+        coyoteTimeCounter = 0;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //¿òÁ÷ÀÓ ÀÔ·Â
+
+        UpdateGroundState();
+
+        //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·ï¿½
         float moveHorIzental = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(moveHorIzental * moveSpeed, rb.velocity.y, moveVertical * moveSpeed);
+        Vector3 movement = new Vector3(moveHorIzental, 0, moveVertical);
 
+        if (movement.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.G) && !isGrounded && gliderTimeLeft > 0)
+        {
+            if (!isGliding)
+            {
+                EnableGlider();
+            }
+            gliderTimeLeft -= Time.deltaTime;
+
+            if (gliderTimeLeft <= 0)
+            {
+                DisableGlider();
+            }
+        }
+        else if (isGliding)
+        {
+            DisableGlider();
+        }
+
+        if (isGliding)
+        {
+            ApplyGliderMovement(moveHorIzental, moveVertical);
+        }
+        else
+        {
+            rb.velocity = new Vector3(moveHorIzental * moveSpeed, rb.velocity.y, moveVertical * moveSpeed);
+
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+        }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
-        }                                                                                                                   
+            realGround = false;
+            coyoteTimeCounter = 0;
+        }
+
+        if (isGrounded)
+        {
+            if (isGliding)
+            {
+                DisableGlider();
+            }
+        }
     }
+
+    void EnableGlider()
+    {
+        isGliding = true;
+
+        if (gliderObject != null)
+        {
+            gliderObject.SetActive(true);
+        }
+
+        rb.velocity = new Vector3(rb.velocity.x, -glidarFallSpeed, rb.velocity.z);
+
+    }
+
+    void DisableGlider()
+    {
+        isGliding = false;
+
+        if (gliderObject != null)
+        {
+            gliderObject.SetActive(false);
+        }
+
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+    }
+
+    void ApplyGliderMovement(float horizontal, float vertical)
+    {
+        Vector3 gliderVelocity = new Vector3(
+            horizontal * gliderMoveSpeed,
+            -glidarFallSpeed,
+            vertical * gliderMoveSpeed
+       );
+
+        rb.velocity = gliderVelocity;
+    }
+
+
+
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -51,12 +170,53 @@ public class PlayerMovement : MonoBehaviour
         {
             coinCount++;
             Destroy(other.gameObject);
-            Debug.Log($"ÄÚÀÎ ¼öÁý : {coinCount}/{totalCoins}");
+            Debug.Log($"ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ : {coinCount}/{totalCoins}");
         }
 
         if (other.gameObject.tag == "Door" && coinCount == totalCoins)
         {
-            Debug.Log("°ÔÀÓ Å¬¸®¾î");
+            Debug.Log("ï¿½ï¿½ï¿½ï¿½ Å¬ï¿½ï¿½ï¿½ï¿½");
+        }
+    }
+
+    void OnCollisionEnter(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            realGround = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            realGround = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            realGround = true;
+        }
+    }
+
+
+
+
+    void UpdateGroundState()
+    {
+        if (realGround)
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+            isGrounded = true;
+        }
+
+        else
+        {
+            isGrounded = false;
         }
     }
 }
